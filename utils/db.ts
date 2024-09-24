ffc344508daa66dac1fea4aa043656b7229f7b59
Akgisno1 <anshulkumargodiwar2001@gmail.com>
2024-09-24 05:34:33 +0530

    fixing db.ts error 1

--- utils/db.ts
+++ utils/db.ts
@@ -1,11 +1,20 @@
+// utils/db.ts
+
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+  mongoose.set("strictQuery", true);
+  if (!process.env.MONGO_URI) {
+    return console.error("MISSING MONGODB_URL");
+  }
+
   try {
     await mongoose.connect(process.env.MONGO_URI!);
     console.log("MongoDB connected");
   } catch (error) {
+    console.error("MongoDB Connection Failed!", error);
     throw new Error("MongoDB Connection Failed!");
   }
 };
+
 export default connectDB;
